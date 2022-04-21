73f4bbf2b609bf9f0ef9b046732aeb76bf08e648
aryde97 <100094373+aryde97@users.noreply.github.com>
2022-04-22 11:03:53 +1200

    Update: added high flow/air in line detection and made fixes to PWM channels to enable full intended operation

--- examples/slf3x-minimal/slf3x-minimal.ino
+++ examples/slf3x-minimal/slf3x-minimal.ino
@@ -40,39 +40,49 @@
 #define MEASURE_DELAY 100
 
 const int PWM_PIN0 = 13;
-const int PWM_PIN1 = 14; 
+const int PWM_PIN1 = 12; 
+
+const bool DEBUG = true;     //SET Debug mode on = true, debug mode off = false, this will affect whether flow sensor error messages affect PWM output frequency
 
 const int BOOT_FREQ = 10;
 
 const int CHANNEL0 = 0; 
-const int CHANNEL1 = 1;
+const int CHANNEL1 = 2;   //Channel 0, 1, share a PWM timer, to make PWM0,PWM1 frequency outputs independent must use separate timer.
 
-const int PWM_RES = 8; 
-const int PWM_DUTY = 127;
+const int PWM_RES = 8;    //Channel resolution will determine operating frequency range  
+const int PWM_DUTY = 127; //Keep Duty 50%, constant for this application
 
-int COUNT0 = 0;
+int COUNT0 = 0;           //Timeout counters, power cycle MCU when I2C communication fails X times.
 int COUNT1 = 0;
 
-int FREQ0 = 0; 
+const int TIMEOUT = 10;   //Number of allowable failed communcation attempts on I2C before power cycle will occur. 
+
+int FREQ0 = 0;          
 int FREQ1 = 0;
 
 // Create first SLF3X.
 const uint8_t SDA_ZERO = 21;
 const uint8_t SCL_ZERO = 22;
+const uint8_t I2C_INDEX0 = 0;
+
 SensirionLF SLF3X_I2C_ZERO(
   SensirionLF::SLF3X_SCALE_FACTOR_FLOW,
   SensirionLF::SLF3X_SCALE_FACTOR_TEMP,
   SensirionLF::SLF3X_I2C_ADDRESS,
+  I2C_INDEX0,
   SCL_ZERO,
   SDA_ZERO);
 
 // Create second SLF3X.
 const uint8_t SDA_ONE = 26;
 const uint8_t SCL_ONE = 25;
+const uint8_t I2C_INDEX1 = 1;
+
 SensirionLF SLF3X_I2C_ONE(
   SensirionLF::SLF3X_SCALE_FACTOR_FLOW,
   SensirionLF::SLF3X_SCALE_FACTOR_TEMP,
   SensirionLF::SLF3X_I2C_ADDRESS,
+  I2C_INDEX1,
   SCL_ONE,
   SDA_ONE);
 
@@ -104,20 +114,39 @@ void loop() {
   if (I2C_RET0 == 0) {
     COUNT0 = 0;
 
-    FREQ0 = round(1000/60 * SLF3X_I2C_ZERO.getFlow())
-    ledcWriteTone(CHANNEL0, FREQ0);
+    FREQ0 = round(10000/60 * SLF3X_I2C_ZERO.getFlow());
 
-    Serial.print("Flow: ");
+    if (FREQ0 < 5) {
+      FREQ0 = 5;
+      Serial.print("FLOW0 LOW");
+    }
+
+    if (!DEBUG) {
+      if (SLF3X_I2C_ZERO.isAirInLineDetected()){
+        FREQ0 = 2000;
+        Serial.print(" [ ERROR : AIR IN LINE 0 ] ");
+      }
+      if (SLF3X_I2C_ZERO.isHighFlowDetected()) {
+        FREQ0 = 10000;
+        Serial.print(" [ ERROR : HIGH FLOW IN LINE 0 ] ");
+      }
+    }
+
+    
+    ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_0, FREQ0);
+
+
+    Serial.print("Flow 0: ");
     Serial.print(SLF3X_I2C_ZERO.getFlow(), 2);
     Serial.print(" ml/min");
 
-    Serial.print(" | Temp: ");
+    Serial.print(" | Temp 0: ");
     Serial.print(SLF3X_I2C_ZERO.getTemp(), 1);
-    Serial.print(" deg C\n");
+    Serial.print(" deg C  |  FREQ0: ");
+    Serial.print(FREQ0);
   } else {
     Serial.print("Error in SLF3X_0.readSample(): ");
-    Serial.println();
-    COUNT0++ 
+    COUNT0++; 
     if (COUNT0 > 9) {
       ESP.restart();
     }
@@ -127,20 +156,38 @@ void loop() {
   if (I2C_RET1 == 0) {
     COUNT1 = 0;
 
-    FREQ1 = round(1000/60 * SLF3X_I2C_ONE.getFlow())
-    ledcWriteTone(CHANNEL0, FREQ0);
+    FREQ1 = round(10000/60 * SLF3X_I2C_ONE.getFlow());
 
-    Serial.print("Flow: ");
+    if (FREQ1 < 5) {
+      FREQ1 = 5;  
+      Serial.print("FLOW1 LOW");
+    } 
+    
+    if (!DEBUG) {
+      if (SLF3X_I2C_ONE.isAirInLineDetected()){
+        FREQ1 = 2000;
+        Serial.print(" [ ERROR : AIR IN LINE 1 ] ");
+      }
+      if (SLF3X_I2C_ONE.isHighFlowDetected()) {
+        FREQ1 = 10000;
+        Serial.print(" [ ERROR : HIGH FLOW IN LINE 1 ] ");
+      }
+    }
+    
+    ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_1, FREQ1);
+
+    Serial.print("   Flow 1: ");
     Serial.print(SLF3X_I2C_ONE.getFlow(), 2);
     Serial.print(" ml/min");
 
-    Serial.print(" | Temp: ");
+    Serial.print(" | Temp 1: ");
     Serial.print(SLF3X_I2C_ONE.getTemp(), 1);
-    Serial.print(" deg C\n");
+    Serial.print(" deg C  |  FREQ 1: ");
+    Serial.println(FREQ1);
   } else {
     Serial.print("Error in SLF3X_1.readSample(): ");
     Serial.println();
-    COUNT1++
+    COUNT1++;
     if (COUNT1 > 9) {
       ESP.restart();
     }
