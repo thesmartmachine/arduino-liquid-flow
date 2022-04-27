94a2e07e1bc782cb152d2544c2122b224d2cf591
aryde97 <100094373+aryde97@users.noreply.github.com>
2022-04-28 09:38:16 +1200

    fix soft-lock, add soft/hard reset
    Previous code set a flag when air in line detected, this would prevent the code from calling .readSample(), in the main loop. as readSample is the function that updates the I2C buffer, the code would have soft-locked when air was detected in-line, this is because when the airInLineDetected() function is called, the function only accesses the most recently read sample from the I2C buffer, which would never update. 
    
    Added the ability for the code to soft-reset the I2C bus if communication fails, and hard reset (restart MCU) if soft reseting does not reestablish connection within 0.5 s.

--- examples/slf3x-minimal/slf3x-minimal.ino
+++ examples/slf3x-minimal/slf3x-minimal.ino
@@ -36,13 +36,13 @@
 #include "driver/ledc.h"
 #include <math.h>
 
-// delay between measurements
-#define MEASURE_DELAY 100
 
+//GLOBAL VARIABLES
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 const int PWM_PIN0 = 13;
 const int PWM_PIN1 = 12; 
 
-const bool DEBUG = true;     //SET Debug mode on = true, debug mode off = false, this will affect whether flow sensor error messages affect PWM output frequency
+const bool DEBUG = true;   //true = serial print operating information
 
 const int CHANNEL0 = 0; 
 const int CHANNEL1 = 2;   //Channel 0, 1, share a PWM timer, to make PWM0,PWM1 frequency outputs independent must use separate timer.
@@ -50,10 +50,8 @@ const int CHANNEL1 = 2;   //Channel 0, 1, share a PWM timer, to make PWM0,PWM1 f
 const int PWM_RES = 8;    //Channel resolution will determine operating frequency range  
 const int PWM_DUTY = 127; //Keep Duty 50%, constant for this application
 
-int COUNT0 = 0;           //Timeout counters, power cycle MCU when I2C communication fails X times.
-int COUNT1 = 0;
-
-const int TIMEOUT = 10;   //Number of allowable failed communcation attempts on I2C before power cycle will occur. 
+int READ0 = 0; 
+int READ1 = 0;
 
 int FREQ0 = 10;          
 int FREQ1 = 10;
@@ -61,7 +59,9 @@ int FREQ1 = 10;
 int MIN_FREQ = 10; 
 int MAX_FREQ = 10000;
 
-// Create first SLF3X.
+
+//CLASS DEFENITIONS
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 const uint8_t SDA_ZERO = 21;
 const uint8_t SCL_ZERO = 22;
 const uint8_t I2C_INDEX0 = 0;
@@ -74,7 +74,6 @@ SensirionLF SLF3X_I2C_ZERO(
   SCL_ZERO,
   SDA_ZERO);
 
-// Create second SLF3X.
 const uint8_t SDA_ONE = 26;
 const uint8_t SCL_ONE = 25;
 const uint8_t I2C_INDEX1 = 1;
@@ -98,8 +97,9 @@ bool no_air_in_line_0 = false;
 #define air_in_line_interval 13 //Time in ms
 unsigned long air_in_line_time = 0;
 bool no_air_in_line_1 = false;
-//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+//SETUP
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 void setup() {
   Serial.begin(115200); // initialize serial communication
 
@@ -118,48 +118,93 @@ void setup() {
   ledcWrite(CHANNEL1, PWM_DUTY);
 }
 
-void loop()
-{
+//MAIN
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+void loop() 
+{    
+  if (millis() > flow_sense_time + flow_sense_time_interval) {
+    READ0 = SLF3X_I2C_ZERO.readSample(); 
+    READ1 = SLF3X_I2C_ONE.readSample();   
+    
+    if (!READ0 && no_air_in_line_0) {
+      FREQ0 = round(10000 / 60 * SLF3X_I2C_ZERO.getFlow());
+      if (SLF3X_I2C_ZERO.isHighFlowDetected()) FREQ0 = 10000;
+    }
+    
+    if(READ0 != 0) {
+      if(DEBUG) Serial.print("Error while reading I2C device 0");
+      if(SLF3X_I2C_ZERO.softReset()) ESP.restart();
+    }
   
-  if (millis() > flow_sense_time + flow_sense_time_interval)
-  {
-    // Update rate liquid flow value and High Flow flag @ 1000Hz
-    if (no_air_in_line_0) {
-      if (SLF3X_I2C_ZERO.readSample() == 0) FREQ0 = round(10000 / 60 * SLF3X_I2C_ZERO.getFlow());
-      if (SLF3X_I2C_ZERO.isHighFlowDetected()) FREQ0 = 10000; }
-
-    if (no_air_in_line_1) {
-      if (SLF3X_I2C_ONE.readSample() == 0) FREQ1 = round(10000 / 60 * SLF3X_I2C_ONE.getFlow());
-      if (SLF3X_I2C_ONE.isHighFlowDetected()) FREQ1 = 10000; }
-
-    flow_sense_time = millis();
+    if (!READ1 && no_air_in_line_1) {
+      FREQ1 = round(1000 / 60 * SLF3X_I2C_ONE.getFlow());
+      if (SLF3X_I2C_ONE.isHighFlowDetected()) FREQ1 = 10000;
+    }
+  
+    if (READ1 != 0) {
+      if(DEBUG) Serial.print("Error while reading I2C device 1");
+      if(SLF3X_I2C_ONE.softReset()) ESP.restart(); 
+    }
   }
-
-  if (millis() > air_in_line_time + air_in_line_interval)
-  {
-    // Update rate temperature value and Air-in-Line flag @ 80Hz
+  
+  if (millis() > flow_sense_time + air_in_line_interval) {
     if (SLF3X_I2C_ZERO.isAirInLineDetected()) {
       no_air_in_line_0 = false;
-      FREQ0 = 2000;
-    } else {
-      no_air_in_line_0 = true;
+      FREQ0 = 2000;  
     }
-
     if (SLF3X_I2C_ONE.isAirInLineDetected()) {
-      no_air_in_line_1 = false;
-      FREQ1 = 2000;
-    } else {
-      no_air_in_line_1 = true;
+      no_air_in_line_1 = true; 
+      FREQ1 = 2000;  
     }
-
-    air_in_line_time = millis();
   }
 
-  // Set PWM outputs
   ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_0, constrain(FREQ0, MIN_FREQ, MAX_FREQ));
   ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_1, constrain(FREQ1, MIN_FREQ, MAX_FREQ));
-  
-  Serial.print(FREQ0);
-  Serial.print(",");
-  Serial.println(FREQ1);
+
+  if(DEBUG) {Serial.print(FREQ0); Serial.print(", "); Serial.println(FREQ1);}
 }
+
+//{
+//  
+//  if (millis() > flow_sense_time + flow_sense_time_interval)
+//  {
+//    // Update rate liquid flow value and High Flow flag @ 1000Hz
+//    if (no_air_in_line_0) {
+//      if (SLF3X_I2C_ZERO.readSample() == 0) FREQ0 = round(10000 / 60 * SLF3X_I2C_ZERO.getFlow());
+//      if (SLF3X_I2C_ZERO.isHighFlowDetected()) FREQ0 = 10000; }
+//
+//    if (no_air_in_line_1) {
+//      if (SLF3X_I2C_ONE.readSample() == 0) FREQ1 = round(10000 / 60 * SLF3X_I2C_ONE.getFlow());
+//      if (SLF3X_I2C_ONE.isHighFlowDetected()) FREQ1 = 10000; }
+//
+//    flow_sense_time = millis();
+//  }
+//
+//  if (millis() > air_in_line_time + air_in_line_interval)
+//  {
+//    // Update rate temperature value and Air-in-Line flag @ 80Hz
+//    if (SLF3X_I2C_ZERO.isAirInLineDetected()) {
+//      no_air_in_line_0 = false;
+//      FREQ0 = 2000;
+//    } else {
+//      no_air_in_line_0 = true;
+//    }
+//
+//    if (SLF3X_I2C_ONE.isAirInLineDetected()) {
+//      no_air_in_line_1 = false;
+//      FREQ1 = 2000;
+//    } else {
+//      no_air_in_line_1 = true;
+//    }
+//
+//    air_in_line_time = millis();
+//  }
+//
+//  // Set PWM outputs
+//  ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_0, constrain(FREQ0, MIN_FREQ, MAX_FREQ));
+//  ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_1, constrain(FREQ1, MIN_FREQ, MAX_FREQ));
+//  
+//  Serial.print(FREQ0);
+//  Serial.print(",");
+//  Serial.println(FREQ1);
+//}
